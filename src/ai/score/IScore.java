623b3ae3a072d6a1f1f91122469b2bbbb3d283f5
Andrii Dychka <andriydychka@gmail.com>
2017-12-15 16:37:52 +0100

    added update score for player method for IScore

--- src/ai/score/IScore.java
+++ src/ai/score/IScore.java
@@ -4,12 +4,21 @@
  */
 package ai.score;
 
+import ai.player.IPlayer;
+
 /**
  *
  * defines the abstraction for the score in the bowling game
  * @author souissi
  */
 public interface IScore {
+    /**
+     * add the points to the total points of the player
+     * @param player 
+     * @param newScore - value which should be added to the total score of the player
+     */
+    void updateForPlayer(IPlayer player, int newScore);
+    
     /**
      * 
      * @return string that represents this score
