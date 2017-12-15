0953e863abf5cc4a20ffca11886d565a067a0394
Souissi Roiya <souissi@ensta.fr>
2017-12-15 12:45:24 +0100

    implemented menu

--- src/agilebowling/AgileBowling.java
+++ src/agilebowling/AgileBowling.java
@@ -28,6 +28,58 @@ public class AgileBowling {
         System.out.print("Mot de passe: ");
         pwd=br.readLine();
         System.out.print("Bienvenu ! ");
+        
+        /////
+        
+        
+        System.out.println("Veuillez choisir une option: ");
+        System.out.println("1- Lancer le jeu");
+        System.out.println("2- Voir les statistiques");
+        System.out.println("3- Quitter");
+        
+        ////////
+        Jeu J=new Jeu();
+        Joueur JR=new Joueur();
+        
+        //////Menu mode de jeu
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+        String choix = br1.readLine();
+        switch(choix)
+        {
+            case "1": 
+            {
+                System.out.println("Veuillez choisir le mode de jeu: ");
+                System.out.println("1- 1 VS 1");
+                System.out.println("2- En équipe");
+                System.out.println("3- Mode tournoi");
+                BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+                String mode = br2.readLine();
+                switch (mode){
+                    case "1": //Niveau de difficulté
+                        {
+                        System.out.println("Veuillez choisir le niveau de difficulté: ");                   
+                        System.out.println("1- Facile (BOB)");
+                        System.out.println("2- Difficile (John)");
+                        BufferedReader br3= new BufferedReader(new InputStreamReader(System.in));
+                        String niv = br3.readLine();
+                        switch (niv){
+                            case "1":JR.Jouer();break;
+                            case "2":JR.Jouer();break;
+                        }
+                    }
+                        break;
+                    case "2": System.out.println("En cours de construction !");break;
+                    case "3": System.out.println("En cours de construction !");break;
+                    
+                }
+                
+            } 
+            break;
+            case "2": J.afficherScore();break;
+            case "3": System.out.println("A bientôt");break;
+        }
+                
+       
     }
     
 }
